e029bbb5365c3c040de5189cd188ced9fe0df089
Caio Davi <caio.davi@tamu.edu>
2020-03-13 16:00:59 -0500

    Selector to case-types
    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,43 @@ import Options from "./Options";
 
 import Slider from "rc-slider";
 import Tooltip from "rc-tooltip";
+import { blue } from "ansi-colors";
 
 const data = window.database.brazil;
 const Handle = Slider.Handle;
 const center = [-15, -52];
+const firstDataOptions = {
+  suspects: {
+    name: "Casos Suspeitos",
+    color: "blue",
+    enabled: false
+  },
+  cases: {
+    name: "Casos Confirmados",
+    color: "red",
+    enabled: false
+  },
+  refuses: {
+    name: "Casos Descartados",
+    color: "green",
+    enabled: false
+  },
+  deaths: {
+    name: "Óbitos",
+    color: "black",
+    enabled: false
+  }
+};
 
 function App() {
   const [day, setDay] = React.useState(data.length - 1);
+  const [dataOptions, setDataOptions] = React.useState(firstDataOptions);
+
+  const toogleDataOption = type => {
+    let newoptions = dataOptions; 
+    newoptions[type].enabled = !dataOptions[type].enabled;
+    setDataOptions({...dataOptions, ...newoptions})
+  }
 
   const handleSlider = props => {
     const { value, dragging, index, ...restProps } = props;
@@ -34,7 +64,13 @@ function App() {
     <div className="App">
       <header className="App-header">COVID-19 Brasil</header>
       <div style={{ display: "flex", flexDirection: "row" }}>
-        <Options data={data} handleSlider={handleSlider} day={data[day].date} />
+        <Options
+          data={data}
+          handleSlider={handleSlider}
+          day={data[day].date}
+          dataOptions={dataOptions}
+          toogleDataOption={toogleDataOption}
+        />
         <div className="leaflet-container">
           <LeafletMap
             center={center}
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -4,18 +4,62 @@ import "rc-slider/assets/index.css";
 import "../styles/Options.css";
 
 const Options = props => {
+  const strike = {
+    textDecorationLine: "line-through",
+    textDecorationStyle: "solid"
+  };
+
+  const textStyle = enabled => {
+    let style = {
+      fontSize: "medium"
+    };
+    if (!enabled) {
+      style["textDecoration"] = "line-through";
+    }
+    return style;
+  };
+
+  const CaseTypes = props => {
+    let optionsCheckBox = [];
+
+    for (let key in props.dataOptions) {
+      const type = props.dataOptions[key];
+      const label = {
+        width: 30,
+        height: 10,
+        backgroundColor: type.color,
+        marginLeft: 15
+      };
+
+      optionsCheckBox.push(
+        <div  className="d-flex flex-row" key={"type_" + key}>
+          <canvas
+            id={"lil_sqr_" + key}
+            style={label}
+            onClick={() => props.toogleDataOption(key)}
+          />
+          <div style={textStyle(type.enabled)}>{type.name}</div>
+        </div>
+      );
+    }
+    return <div>{optionsCheckBox}</div>;
+  };
+
   return (
     <div className="Options">
       <div className="option">
-      Day: {props.day}
-      <Slider
-        min={0}
-        max={props.data.length - 1}
-        defaultValue={props.data.length - 1}
-        handle={props.handleSlider}
-      />
+        Day: {props.day}
+        <Slider
+          min={0}
+          max={props.data.length - 1}
+          defaultValue={props.data.length - 1}
+          handle={props.handleSlider}
+        />
+        <CaseTypes
+          dataOptions={props.dataOptions}
+          toogleDataOption={props.toogleDataOption}
+        />
       </div>
-      
     </div>
   );
 };
