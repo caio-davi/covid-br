108a348c3080f04b7f22c57160b7a9970281dad0
Caio Davi <caio.davi@tamu.edu>
2020-03-13 19:01:08 -0500

    Updates

---
+++ precache-manifest.d8818eafb9cd08c613e01aa29b9432e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c755425210142632cd5f1420cdb05207",
+    "url": "/covid-br/index.html"
+  },
+  {
+    "revision": "78eaaf0168f464d90de0",
+    "url": "/covid-br/static/css/2.1c7e3d2b.chunk.css"
+  },
+  {
+    "revision": "a2920c8fe39054490aba",
+    "url": "/covid-br/static/css/main.814023e0.chunk.css"
+  },
+  {
+    "revision": "78eaaf0168f464d90de0",
+    "url": "/covid-br/static/js/2.4ad82002.chunk.js"
+  },
+  {
+    "revision": "18e4c7d63bfbf17f55f88bea26ef42f5",
+    "url": "/covid-br/static/js/2.4ad82002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2920c8fe39054490aba",
+    "url": "/covid-br/static/js/main.d34dcfe4.chunk.js"
+  },
+  {
+    "revision": "9146151d77ddeb3d5d62",
+    "url": "/covid-br/static/js/runtime-main.3740260d.js"
+  }
+]);
